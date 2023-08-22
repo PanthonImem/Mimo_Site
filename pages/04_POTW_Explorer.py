927a0f4ac07761157a6ce1bb020dd5aa3314da76
PanthonImem <40337545+PanthonImem@users.noreply.github.com>
2023-08-22 19:18:51 +0700

    add by position

--- pages/04_POTW_Explorer.py
+++ pages/04_POTW_Explorer.py
@@ -4,7 +4,38 @@ import streamlit as st
 
 import warnings
 warnings.filterwarnings('ignore')
-
+activate_dict = {
+    'Goal Poacher': ['CF'],
+    'Fox in the Box': ['CF'],
+    'Roaming Flank': ['RWF','LWF','RMF','LMF'],
+    'Creative Playmaker':['SS','RWF','LWF','AMF','RMF','LMF'],
+    'Deep-Lying Forward':['CF','SS'], 
+    'Orchestrator':['CMF','DMF'], 
+    'Prolific Winger':['RWF','LWF'],
+    'Full-back Finisher':['RB','LB'], 
+    'Defensive Goalkeeper':['GK'], 
+    'Offensive Goalkeeper':['GK'], 
+    'The Destroyer':['CMF','DMF','CB'], 
+    'Build Up':['CB'],
+    'Box-to-Box':['RMF','LMF','CMF','DMF'],
+    'Anchor Man':['DMF'], 
+    'Cross Specialist':['RWF','LWF','RMF','LMF'], 
+    'Hole Player':['SS','AMF','RMF','LMF','CMF'],
+    'Offensive Full-back':['RB','LB'], 
+    'Dummy Runner':['CF','SS','AMF'],
+    'Fox in the Box':['CF'],
+    'Extra Frontman':['CB'], 
+    'Target Man':['CF'], 
+    'Classic No. 10':['SS','AMF','CMF'],
+    'Defensive Full-back':['RB','LB'], 
+    '---':[],
+}
+def get_unactivated(pos):
+    ls = []
+    for key,value in activate_dict.items():
+        if(pos not in value):
+            ls.append(key)
+    return ls
 
 def main():
     st.set_page_config(layout="wide")
@@ -15,7 +46,7 @@ def main():
 
     st.write("Mimo Value Index is a made-up index for determining usefulness of a POTW player.")
 
-    st.write("Overall Rating, How much of an upgrade from base, and Form are taken into account")
+    st.write("Overall Rating, How much of an upgrade from base, Super-sub, and Form are taken into account")
 
     potw_packs = [i for i in adf['pack'].unique() if 'POTW' in i]
 
@@ -26,16 +57,16 @@ def main():
 
     pdf = pdf.merge(bdf, on = 'Player Name', how = 'left')
     pdf['base_rating'] = pdf['base_rating'].fillna(87)
-    pdf['mimo_stat_value_index'] = np.clip(pdf['max_ovr_rating']-92, 0, 10)/10
+    pdf['mimo_stat_value_index'] = np.clip(pdf['max_ovr_rating']-92, 0, 10)
     pdf['mimo_upgrade_value_index'] = 0.7+0.3*np.clip(pdf['max_ovr_rating']-pdf['base_rating'],0,3)/3
 
     pdf['mimo_form_value_index'] = 1
     pdf.loc[pdf.Form == 'Standard', 'mimo_form_value_index'] = 0.8
     pdf.loc[pdf.Form == 'Inconsistent', 'mimo_form_value_index'] = 0.5
     pdf['mimo_value_index'] = np.clip(pdf['mimo_upgrade_value_index']*pdf['mimo_stat_value_index']\
-    *pdf['mimo_form_value_index']+0.1*pdf['s_Super-sub'],0.05,np.inf).round(2)
+    *pdf['mimo_form_value_index']+1*pdf['s_Super-sub'],0.5,np.inf).round(1)
 
-    gdf = pdf.groupby('pack').agg({'mimo_value_index':'mean'}).sort_values('mimo_value_index',ascending = False).round(3).reset_index()
+    gdf = pdf.groupby('pack').agg({'mimo_value_index':'mean'}).sort_values('mimo_value_index',ascending = False).round(2).reset_index()
     
     mmean = gdf['mimo_value_index'].mean().round(2)
     mstd = gdf['mimo_value_index'].std().round(2)
@@ -65,12 +96,24 @@ def main():
         st.write("At Mimo Value Index of {}, {} is {} as a POTW pack".format(pack_val, pack, ttext))
         st.write(df)
 
-    st.write(pdf[['pack','Player Name','max_ovr_rating','max_position','Playstyle','Form','mimo_value_index']]\
-    .sort_values('mimo_value_index', ascending = False).reset_index(drop = True))
+
+    
+    st.write('Best POTW Players by Position')
+    with sc1:
+        pos = st.selectbox('Position Filter', list((adf['Position'].unique())))
+    with sc2:
+        activated = st.checkbox('Show Only Players with Activated Playstyle')
+
+    flag_playstyle = True
+    if activated:
+        flag_playstyle = ~pdf['Playstyle'].isin(get_unactivated(pos))
+    df = pdf[(pdf['Possible Positions'].str.contains(pos))&(flag_playstyle)]
+
+    df['mimo_value_index_'+pos] = np.clip(np.clip(df['or_'+pos]-92, 0, 10)
+    *df['mimo_form_value_index']+1*df['s_Super-sub'],0.5,np.inf).round(1)
+    st.write(df[['pack','Player Name','or_'+pos,'max_ovr_rating','max_position','Playstyle','Form','mimo_value_index_'+pos]]\
+    .sort_values('mimo_value_index_'+pos, ascending = False).reset_index(drop = True))
 
    
-            
-    
-
 if __name__ == "__main__":
     main()
