0d0962a92f499800a870a42d7526a8cd9547b134
PanthonImem <40337545+PanthonImem@users.noreply.github.com>
2023-09-18 01:13:40 -0400

    add class for button to HTML

--- code/utility.py
+++ code/utility.py
@@ -6,7 +6,7 @@ def hide_github():
         <style>
         .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
         .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK {
+        .viewerBadge_text__1JaDK, css-ztfqz8 ef3psqc4, {
             display: none;
         }
         </style>
