51a1bfb2ba704aca746604442923b2cccccef679
PanthonImem <40337545+PanthonImem@users.noreply.github.com>
2023-09-18 01:19:31 -0400

    minorupdate

--- code/utility.py
+++ code/utility.py
@@ -6,10 +6,15 @@ def hide_github():
         <style>
         .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
         .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-        .viewerBadge_text__1JaDK, .css-ztfqz8, {
+        .viewerBadge_text__1JaDK{
+            display: none;
+        }
+        button.css-ztfqz8.ef3psqc4 {
             display: none;
         }
         </style>
+
+
         """,
         unsafe_allow_html=True
     )
